86b364c8371b1f7f1e724f26b8972954ea26699d
Gabor Sebastian <sebi.gabor20@outlook.com>
2024-03-14 23:42:44 +0200

    Added pwa functionality

---
+++ pwa/code/app-about-54b3f4dd.js
@@ -0,0 +1,27 @@
+import{i as n,s as c,x as o,a as d,e as u}from"./index-c880d41f.js";const m=n`
+  @media(min-width: 1000px) {
+    sl-card {
+      max-width: 70vw;
+    }
+  }
+
+  main {
+    margin-left: 25px;
+  }
+`;var f=Object.defineProperty,h=Object.getOwnPropertyDescriptor,v=(l,r,p,a)=>{for(var e=a>1?void 0:a?h(r,p):r,i=l.length-1,t;i>=0;i--)(t=l[i])&&(e=(a?t(r,p,e):t(e))||e);return a&&e&&f(r,p,e),e};let s=class extends c{render(){return o`
+      <app-header ?enableBack="${!0}"></app-header>
+
+      <main>
+        <h2>Despre</h2>
+
+        <sl-card>
+          <h2>Despre aplicație</h2>
+
+          <p>IntelliHosp este un proiect pentru gestionarea fișelor de îngrijiri ale pacienților din spitale.
+          Aplicația este dezvoltată în primă fază de către Sebastian Gabor în cadrul proiectului de licență.</p>
+          </p>
+
+        </sl-card>
+      </main>
+    `}};s.styles=[d,m];s=v([u("app-about")],s);export{s as AppAbout};
+//# sourceMappingURL=app-about-54b3f4dd.js.map
